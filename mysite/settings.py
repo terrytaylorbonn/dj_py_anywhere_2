0e6577e6b0025d0e3e44e1bbd18a291c143355ba
T Taylor <terrytaylorbonn@gmail.com>
2024-11-23 14:06:36 -0500

    ch4 add swagger

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
+    'drf_yasg',
+    'post',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Posts API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('api/v1/', 
+        include([
+            path('post/', include(('post.api.urls', 'post'), namespace='posts')),
+           # path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+            path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
+        ])
+    ),
 ]

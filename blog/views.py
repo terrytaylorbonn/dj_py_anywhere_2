bbadc88729f871c6b421f91f21d4d0d5f4e28c9a
T Taylor <terrytaylorbonn@gmail.com>
2024-12-06 18:54:38 -0500

    publish branch8 (last lesson)

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import CommentForm, PostForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -65,3 +65,31 @@ def post_remove(request, pk):
     if request.method == 'POST':
         post.delete() #built in method/function from django
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    #if request.method == 'POST':
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)  
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    #post_pk = comment.post.pk
+    #if request.method == 'POST':
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
